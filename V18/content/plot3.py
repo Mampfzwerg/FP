fe1edfb6d3e410f02ea619fa28cf0cce57899808
Marek Karzel <karzel.marek@udo.edu>
2020-06-12 11:59:54 +0200

    Plots

---
+++ V18/content/plot3.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.genfromtxt('mess3.txt', unpack=True)
+x = np.arange(0, 8192)
+#params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
+#
+#errors = np.sqrt(np.diag(covariance_matrix))
+#
+#print('a = {:.3f} ± {:.4f}'.format(params[0], errors[0]))
+#print('b = {:.3f} ± {:.4f}'.format(params[1], errors[1]))
+#
+#def gerade (x, m, b):
+#    return m*x+b
+#
+#z = np.linspace(np.min(x) - 5e-3, np.max(x) + 5e-3)
+
+plt.plot(x, y, 'b-', label='3')
+#plt.plot(z, gerade (z, *params), 'b-', label='Ausgleichsgerade')
+plt.xlabel(r'Counts')
+plt.ylabel(r'Channel')
+plt.legend(loc='best')
+
+plt.tight_layout()
+plt.savefig('plot3.pdf')
