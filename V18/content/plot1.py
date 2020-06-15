032074f8021ca1571843c71126d2397c3812b628
Marek Karzel <karzel.marek@udo.edu>
2020-06-15 03:19:17 +0200

    Kalibrierung

--- V18/content/plot1.py
+++ V18/content/plot1.py
@@ -1,22 +1,162 @@
-import matplotlib.pyplot as plt
+
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
 
 y = np.genfromtxt('mess1.txt', unpack=True)
-x = np.arange(0, 8192)
-#params, covariance_matrix = np.polyfit(x, y, deg=1, cov=True)
-#
-#errors = np.sqrt(np.diag(covariance_matrix))
-#
-#print('a = {:.3f} ± {:.4f}'.format(params[0], errors[0]))
-#print('b = {:.3f} ± {:.4f}'.format(params[1], errors[1]))
-#
-#def gerade (x, m, b):
-#    return m*x+b
-#
-#z = np.linspace(np.min(x) - 5e-3, np.max(x) + 5e-3)
-
+x = np.arange(y.size)
 plt.plot(x, y, 'b-', label='152Eu')
-#plt.plot(z, gerade (z, *params), 'b-', label='Ausgleichsgerade')
+
+def gauss(x, m, a, b, c):
+    return a * np.exp(-(x-m)**2 / b) + c
+
+
+x1 = np.arange(200, 400)
+y1 = y[200:400]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[320, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-', label='Gaussian Fits')
+
+
+
+x1 = np.arange(550, 670)
+y1 = y[550:670]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[610, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(750, 950)
+y1 = y[750:950]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[850, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(1000, 1050)
+y1 = y[1000:1050]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[1025, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(1050, 1150)
+y1 = y[1050:1150]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[1100, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(1850, 2050)
+y1 = y[1850:2050]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[1950, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(2100, 2200)
+y1 = y[2100:2200]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[2150, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(2350, 2450)
+y1 = y[2350:2450]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[2400, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(2650, 2750)
+y1 = y[2650:2750]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[2700, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(2750, 2800)
+y1 = y[2750:2800]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[2775, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+
+x1 = np.arange(3450, 3550)
+y1 = y[3450:3550]
+par, cov = optimize.curve_fit(gauss, x1, y1, p0=[3500, 10000, 100, 1])
+
+m = ufloat(par[0], np.sqrt(cov[0][0]))
+a = ufloat(par[1], np.sqrt(cov[1][1]))
+b = ufloat(par[2], np.sqrt(cov[2][2]))
+c = ufloat(par[3], np.sqrt(cov[3][3]))
+print(m, a, b, c)
+plt.plot(x1, gauss(x1, *par), 'r-')
+
+
+plt.xlim(0, 4000)
 plt.xlabel(r'Counts')
 plt.ylabel(r'Channel')
 plt.legend(loc='best')
---
+++ V18/content/plot5.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+E = np.array([121.78, 244.70, 344.30, 411.12, 443.96, 778.90, 867.37, 964.08, 1085.90, 1112.10, 1408.00])
+x = np.array([ 308.80, 613.80, 860.70, 1026.51, 1107.93, 1938.62, 2158.39, 2398.18, 2700.41, 2765.02, 3499.69])
+
+params, covariance_matrix = np.polyfit(x, E, deg=1, cov=True)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+print('a = {:.3f} ± {:.4f}'.format(params[0], errors[0]))
+print('b = {:.3f} ± {:.4f}'.format(params[1], errors[1]))
+
+def gerade (x, m, b):
+    return m*x+b
+
+z = np.linspace(np.min(x) - 100, np.max(x) + 100)
+
+plt.plot(x, E, 'bx', label='Data')
+plt.plot(z, gerade (z, *params), 'r-', label='Linear Regression')
+plt.xlabel(r'$\mu_i$')
+plt.ylabel(r'$E_i \: / \: keV$')
+plt.legend(loc='best')
+
+plt.tight_layout()
+plt.savefig('plot5.pdf')
