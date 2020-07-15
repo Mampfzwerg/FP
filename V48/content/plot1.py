efc1b4ee475db6b67bb427b7dad8e9d6f30eae58
Marek Karzel <karzel.marek@udo.edu>
2020-07-15 19:58:54 +0200

    ausw48.2

--- V48/content/plot1.py
+++ V48/content/plot1.py
@@ -7,15 +7,16 @@ import uncertainties.unumpy as unp
 
 T, I = np.genfromtxt('mess1.txt', unpack=True)
 T = T + 273.15
-ln = np.log(abs(I))
+I = np.abs(I)
 
 Tx = T[9:30]
 Ix = I[9:30]
+print(T[9], T[30])
 
 def f(x, a, b):
     return np.exp(a/x) * b
 
-params, cov = curve_fit(f, Tx, Ix, p0=[-6000, 1])
+params, cov = curve_fit(f, Tx, Ix) #, p0=[-6000, 1])
 err = np.sqrt(np.diag(cov))
 
 # Parameter
@@ -23,12 +24,19 @@ a = ufloat(params[0], err[0])
 b = ufloat(params[1], err[1])
 print(a, b)
 
+kB = 8.617333262e-5
+W = -a * kB
+print(kB, W)
+
+
 z = np.linspace(T[9], T[30], 500)
 
-
+plt.plot(T, I, 'x', color='#18c8fc', label='Messwerte')
+plt.plot(Tx, Ix, '.', color='#1891fc', label=' Approximierte Messwerte')
 plt.plot(z, f(z, *params), 'b-', label='Exponentielle Approximation')
-plt.plot(T, ln, 'cx', label='Messwerte')
-#plt.xlim(0, 4000)
+plt.ylim(1e-2, 10)
+plt.yscale('log')
+plt.grid(linestyle='dotted', which="both")
 plt.xlabel(r'$T/K$')
 plt.ylabel(r'ln($I$)')
 plt.legend(loc='best')
--- V48/content/plot2.py
+++ V48/content/plot2.py
@@ -6,11 +6,37 @@ from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
 T, I = np.genfromtxt('mess2.txt', unpack=True)
-ln = np.log(abs(I))
+T = T + 273.15
+I = np.abs(I)
+
+Tx = T[12:32]
+Ix = I[12:32]
+print(T[12], T[32])
+
+def f(x, a, b):
+    return np.exp(a/x) * b
+
+params, cov = curve_fit(f, Tx, Ix) #, p0=[-6000, 1])
+err = np.sqrt(np.diag(cov))
+
+# Parameter
+a = ufloat(params[0], err[0])
+b = ufloat(params[1], err[1])
+print(a, b)
+
+kB = 8.617333262e-5
+W = -a * kB
+print(kB, W)
 
 
-plt.plot(T, ln, 'cx', label='Messwerte')
-#plt.xlim(0, 4000)
+z = np.linspace(T[12], T[32], 500)
+
+plt.plot(T, I, 'x', color='#18c8fc', label='Messwerte')
+plt.plot(Tx, Ix, '.', color='#1891fc', label=' Approximierte Messwerte')
+plt.plot(z, f(z, *params), 'b-', label='Exponentielle Approximation')
+plt.ylim(1e-2, 10)
+plt.yscale('log')
+plt.grid(linestyle='dotted', which="both")
 plt.xlabel(r'$T/K$')
 plt.ylabel(r'ln($I$)')
 plt.legend(loc='best')
